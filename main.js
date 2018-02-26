716d81572aac5c4af41b1f427a31703074d18432
mitchreece <mitchellreece18@gmail.com>
2018-02-26 12:21:42 +0000

    setup electron

---
+++ main.js
@@ -0,0 +1,17 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+app.on('ready', () => {
+	win = new BrowserWindow({
+		backgroundColor: '#000',
+	})
+
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true,
+	}))
+})
