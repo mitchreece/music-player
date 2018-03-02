24e655fb8f7b005149c78bda8ec5fb604c2e4aff
mitchreece <mitchellreece18@gmail.com>
2018-03-02 17:22:14 +0000

    install react devtools

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron')
 const { enableLiveReload } = require('electron-compile')
+const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer')
 const path = require('path')
 const url = require('url')
 
@@ -9,9 +10,14 @@ let win
 enableLiveReload({strategy: 'react-hmr'})
 
 app.on('ready', () => {
+	installExtension(REACT_DEVELOPER_TOOLS)
 
 	win = new BrowserWindow({
+		height: 650,
+		width: 1000,
 		backgroundColor: '#000',
+		titleBarStyle: 'hidden-inset',
+		fullscreenWindowTitle: true,
 	})
 
 	win.loadURL(url.format({
