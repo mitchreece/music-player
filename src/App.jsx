62b4b220247090f9fbdfb6419efe018fbbade416
mitchreece <mitchellreece18@gmail.com>
2018-03-05 02:28:32 +0000

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { MemoryRouter as Router } from 'react-router-dom'
 import TitleBar from './components/TitleBar'
 
 export default class App extends React.Component {
 
 	render() {
-		return <TitleBar />
+		return (
+			<Router>
+				<TitleBar />
+			</Router>
+		)
 	}
 
 }
